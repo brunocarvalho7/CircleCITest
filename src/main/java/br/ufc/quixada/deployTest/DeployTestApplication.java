10429341e82b5a6aca9ed22b569b98a995e14924
Bruno Carvalho <brunocarvalho287@gmail.com>
2018-09-22 15:38:13 -0300

    First Commit

---
+++ src/main/java/br/ufc/quixada/deployTest/DeployTestApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.deployTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployTestApplication {
+
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(DeployTestApplication.class, args);
+	}
+}
