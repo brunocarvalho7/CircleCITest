f8de7ae170bd2e5395ab2b1be9b781f36f67d9a6
Bruno Carvalho <brunocarvalho287@gmail.com>
2018-09-24 15:35:49 -0300

    Fix issues - No Loader homepage

--- src/main/java/br/ufc/quixada/deployTest/DeployTestApplication.java
+++ src/main/java/br/ufc/quixada/deployTest/DeployTestApplication.java
@@ -2,9 +2,16 @@ package br.ufc.quixada.deployTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeployTestApplication {
+public class DeployTestApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DeployTestApplication.class);
+	}
 
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(DeployTestApplication.class, args);
