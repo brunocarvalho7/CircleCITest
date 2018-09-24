eada43f9f3c5341b9a142e0c2a2dbc9e0f9f6f29
Bruno Carvalho <brunocarvalho287@gmail.com>
2018-09-24 15:43:06 -0300

    adding ola page

--- src/main/java/br/ufc/quixada/deployTest/controller/HomeController.java
+++ src/main/java/br/ufc/quixada/deployTest/controller/HomeController.java
@@ -1,6 +1,7 @@
 package br.ufc.quixada.deployTest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class HomeController {
     @RequestMapping("/")
     public String hello(){
         return "index.html";
+    }
+
+    @GetMapping("/ola")
+    public String ola(){
+        return "ola.html";
     }
 }
